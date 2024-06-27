c09a5661bcfb9e901fda91ab85f42f3a69fe9a0b
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-06-27 13:31:52 +0200

    trayectoria add

--- src/app/components/trayectoria/trayectoria.component.ts
+++ src/app/components/trayectoria/trayectoria.component.ts
@@ -5,8 +5,21 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './trayectoria.component.html',
-  styleUrl: './trayectoria.component.css'
+  styleUrl: './trayectoria.component.css',
 })
 export class TrayectoriaComponent {
-
+  eventos = [
+    {
+      title: 'Nace IWF Argentina',
+      year: '2004',
+      description:
+        'IWF establece base en nuestro país. Mujeres de distintos ámbitos, reconocidas profesionales y protagonistas en su acción transformadora en la sociedad, iniciaron la trayectoria de IWF Argentina',
+    },
+    {
+      title: 'EL DESAFÍO DE CONSTRUIR EL BIEN COMÚN',
+      year: '2006',
+      description:
+        'Diálogo abierto y plural con las candidatas a legisladoras y políticas. Elisa Carrió, Norma Morandini, Patricia Bullrich, Hilda “Chiche” Duhalde, Vilma Ripoll, Marta Oyhanarte.',
+    },
+  ];
 }
