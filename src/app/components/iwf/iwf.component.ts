c837aa3b62ff14165687f0f973346e467adc002a
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-06-27 13:11:38 +0200

    iwf component

--- src/app/components/iwf/iwf.component.ts
+++ src/app/components/iwf/iwf.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { Member } from '../../interface';
 
 @Component({
   selector: 'app-iwf',
   standalone: true,
   imports: [],
   templateUrl: './iwf.component.html',
-  styleUrl: './iwf.component.css'
+  styleUrl: './iwf.component.css',
 })
 export class IwfComponent {
+  autoridades: Member[] = [
+    {
+      name: 'Graciela Romer',
+      title: 'Vocal Suplente',
+    },
+    {
+      name: 'Silvia Fesquest',
+      title: 'Presidenta',
+    },
+  ];
 
+  socias: Member[] = [
+    {
+      name: 'Marta Cristina Antonelli',
+      title:
+        'Investigadora principal - Instituto de Biología Celular y Neurociencias - Facultad de Medicina UBA - CONICET',
+    },
+    {
+      name: 'Claudia Alvarez Arguello',
+      title: 'Vicepresidente y CEO Alvarez Arguelles Hoteles',
+    },
+  ];
 }
