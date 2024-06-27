f7dd2deae07a1b45739c8556c7365c6f60d3362c
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-06-27 10:05:23 +0200

    components and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NoticiasComponent } from './components/noticias/noticias.component';
+import { ActividadesComponent } from './components/actividades/actividades.component';
+import { TrayectoriaComponent } from './components/trayectoria/trayectoria.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'noticias', component: NoticiasComponent },
+  { path: 'actividades', component: ActividadesComponent },
+  { path: 'trayectoria', component: TrayectoriaComponent },
+];
---
+++ src/app/components/trayectoria/trayectoria.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trayectoria',
+  standalone: true,
+  imports: [],
+  templateUrl: './trayectoria.component.html',
+  styleUrl: './trayectoria.component.css'
+})
+export class TrayectoriaComponent {
+
+}
