8f27e7e186a52af9b17613445ab58f10f1aeb91c
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-07-22 10:54:44 +0200

    navbar styleguide

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,24 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    colors: {
+      orangeiw: "#E76503",
+      lightorangeiw: "#FCA43C",
+      lightbrowniw: "#D3C587",
+      blueiw: "#174FB7",
+      darkblueiw: "#213143",
+      purpleiw: "#9B0062",
+      darkpurpleiw: "#76004A",
+      pasteliw: "#F5F1EC",
+      whiteiw: "#FCFCFC",
+      grayiw: "#726D67",
+      darkgrayiw: "#453F39",
+    },
     extend: {
       fontFamily: {
-        sans: ["Inter Tight", ...defaultTheme.fontFamily.sans],
-        serif: ["Cardo", ...defaultTheme.fontFamily.sans],
+        sans: ["Sweet Sans Pro", ...defaultTheme.fontFamily.sans],
+        serif: ["Adobe Caslon Pro", ...defaultTheme.fontFamily.sans],
+        body: ["Helvetica Neue"],
       },
     },
   },
